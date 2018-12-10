2166060134658a675093fe3c6a63a76a0de6de67
Gliga Dumitru <gliga_dumitru@yahoo.com>
2018-12-10 23:16:45 +0200

    navbar and search input done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from "./components/navbar/NavBar";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      age: '',
+      numberOfImages: 3,
+      apiUrl: 'https://pixabay.com/api',
+      apiKey: '10934107-8af2a8ca536178650b1ab0efc',
+      images: []
+    }
+  }
+
+
+  handleImages = () => {
+    console.log("a");
+    axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}?q=${this.state.searchText}&image_type=photo&per_page=${this.state.numberOfImages}&safesearch=true`)
+    .then(res => this.setState({ images: res.data.hits }))
+    .catch(err => console.log(err));
+  }
+
+  onTextChange = ({ target }) => {
+
+    this.setState({
+      searchText: target.value
+    }, this.handleImages);
+  }
+
+  
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value },this.handleImages);
+  };
+
+
+
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <MuiThemeProvider>
+          <NavBar
+            numberOfImages={this.state.numberOfImages}
+            searchText={this.statesearchText}
+            handleChange={this.handleChange}
+            onTextChange={this.onTextChange}
+          />
+          {JSON.stringify(this.state)}
+        </MuiThemeProvider>
       </div>
     );
   }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import SearchIcon from '@material-ui/icons/Search';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit,
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
+    },
+});
+
+
+const Search = (props) => {
+
+
+
+
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                        {/* <MenuIcon /> */}
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                        Material-UI
+                        </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            value={props.searchText}
+                            onChange={props.onTextChange}
+                        />
+                    </div>
+                    <div className={classes.search}>
+                        <Select
+                            value={props.numberOfImages}
+                            onChange={props.handleChange}
+                            inputProps={{
+                                name: 'numberOfImages',
+                                id: "numberOfImagesID"
+                            }}
+                        >
+                            <MenuItem value={0}>Zero</MenuItem>
+                            <MenuItem value={10}>10</MenuItem>
+                            <MenuItem value={20}>20</MenuItem>
+                            <MenuItem value={30}>30</MenuItem>
+                        </Select>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div >
+    );
+
+}
+
+
+
+export default withStyles(styles)(Search);
